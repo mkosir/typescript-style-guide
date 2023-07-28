ec7547001aefb503a0fcf62e7478f4065bfc75db
mkosir <kosir.mar@gmail.com>
2023-07-28 18:22:44 +0200

    update docusaurus

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -57,6 +57,10 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: "dark",
+        respectPrefersColorScheme: false,
+      },
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
