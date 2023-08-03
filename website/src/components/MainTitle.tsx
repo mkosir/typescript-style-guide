c29f581c3bde841547920550c178bd0d38b21d0f
mkosir <kosir.mar@gmail.com>
2023-08-03 17:31:46 +0200

    update docusaurus

--- website/src/components/MainTitle.tsx
+++ website/src/components/MainTitle.tsx
@@ -7,8 +7,24 @@ type MainTitleProps = {
 
 export const MainTitle = ({ children }: MainTitleProps) => {
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <Tilt tiltMaxAngleX={8} tiltMaxAngleY={5} transitionSpeed={2500}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        marginBottom: "30px",
+      }}
+    >
+      <Tilt
+        tiltMaxAngleX={8}
+        tiltMaxAngleY={5}
+        transitionSpeed={2500}
+        // glareEnable={true}
+        // glareMaxOpacity={0.1}
+        // glareColor="#ffffff"
+        // glareColor="#296bb3"
+        // glarePosition="all"
+        // glareBorderRadius="5px"
+      >
         <div
           style={{ display: "flex", alignItems: "center", columnGap: "6px" }}
         >
