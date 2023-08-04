555540d3b6713f1f3b87eee87d22b1ef964938b5
mkosir <kosir.mar@gmail.com>
2023-08-04 14:03:50 +0200

    update docusaurus

--- website/src/components/TableOfContents.tsx
+++ website/src/components/TableOfContents.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
+import { TOCItem } from "@docusaurus/mdx-loader";
 import TOCInline from "@theme/TOCInline";
 
-export const TableOfContents = ({ items }: { items: any }) => {
+export const TableOfContents = ({
+  items,
+}: {
+  items: ReadonlyArray<TOCItem>;
+}) => {
   return <TOCInline toc={items} minHeadingLevel={2} maxHeadingLevel={3} />;
 };
