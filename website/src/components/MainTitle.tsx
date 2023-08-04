de4fa88b3e7acb4d4863f8b56caa25fc4cc6f0cc
mkosir <kosir.mar@gmail.com>
2023-08-04 22:33:31 +0200

    update docusaurus

--- website/src/components/MainTitle.tsx
+++ website/src/components/MainTitle.tsx
@@ -31,7 +31,7 @@ export const MainTitle = ({ children }: MainTitleProps) => {
           style={{ display: "flex", alignItems: "center", columnGap: "6px" }}
         >
           <img src="https://raw.githubusercontent.com/mkosir/typescript-style-guide/main/misc/typescript-logo-30.png" />{" "}
-          <h1 style={{ marginBottom: 0 }}>{children}</h1>
+          <h1>{children}</h1>
         </div>
       </Tilt>
     </div>
