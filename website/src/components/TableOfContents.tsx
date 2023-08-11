98a133196a343ccf974d9449603124dd5f4c8fff
mkosir <kosir.mar@gmail.com>
2023-08-11 17:28:42 +0200

    update docusaurus

--- website/src/components/TableOfContents.tsx
+++ website/src/components/TableOfContents.tsx
@@ -7,13 +7,13 @@ export const TableOfContents = ({ items }: { items: ReadonlyArray<TOCItem> }) =>
   const [isTocExpanded, setIsTocExpanded] = useState(false);
 
   return (
-    <div className="toc-inline mb-4">
-      <TOCInline toc={items} minHeadingLevel={2} maxHeadingLevel={isTocExpanded ? 3 : 2} />
+    <div className="toc-inline -mt-2">
       <button
-        className="text-[11px] hover:cursor-pointer"
+        className="px-1 text-[11px] hover:cursor-pointer"
         onClick={() => setIsTocExpanded((prev) => !prev)}
         title={`${isTocExpanded ? 'Collapse' : 'Expand'} - Table of Contents`}
       >{`${isTocExpanded ? 'Collapse' : 'Expand'} - TOC`}</button>
+      <TOCInline toc={items} minHeadingLevel={2} maxHeadingLevel={isTocExpanded ? 3 : 2} />
     </div>
   );
 };
