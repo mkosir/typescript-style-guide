db69b7e7e627c3fac09b6c4d2c056049d7a87e1c
mkosir <kosir.mar@gmail.com>
2024-03-08 11:46:18 +0100

    update docusaurus

--- website/src/components/GithubStats.tsx
+++ website/src/components/GithubStats.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const DOCUSAURUS_LG_BREAKPOINT = 997;
-
 export const GithubStats = () => {
   return (
     <div className="flex items-center gap-0.5">
@@ -9,13 +7,13 @@ export const GithubStats = () => {
         src="https://ghbtns.com/github-btn.html?user=mkosir&amp;repo=typescript-style-guide&amp;type=star&amp;count=true&amp;size=small"
         width={100}
         height={20}
-        className={`mr-10 md:mr-40 min-[${DOCUSAURUS_LG_BREAKPOINT}px]:mr-0`} // Adjust because of swizzling - src/theme/NavbarItem/ComponentTypes.tsx
+        className="mr-10 md:mr-40 min-[997px]:mr-0" // Adjust because of swizzling - src/theme/NavbarItem/ComponentTypes.tsx
       />
       <iframe
         src="https://ghbtns.com/github-btn.html?user=mkosir&amp;repo=typescript-style-guide&amp;type=fork&amp;count=true&amp;size=small"
         width={100}
         height={20}
-        className={`hidden min-[${DOCUSAURUS_LG_BREAKPOINT}px]:inline`}
+        className="hidden min-[997px]:inline"
       />
     </div>
   );
