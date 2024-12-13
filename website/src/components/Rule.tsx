fb5e8eb84353bc806eea3550ac5635cbb761a445
mkosir <kosir.mar@gmail.com>
2024-12-13 10:58:31 +0100

    update docusaurus

--- website/src/components/Rule.tsx
+++ website/src/components/Rule.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import AnimateHeight from 'react-animate-height';
 import CodeBlock from '@theme/CodeBlock';
+import clsx from 'clsx';
 
 type RuleProps = {
   children: string;
@@ -19,7 +20,17 @@ export const Rule = ({ children, href, prefix }: RuleProps) => {
           className="mb-1 inline-flex cursor-pointer items-center rounded-md bg-gray-100 px-1.5 py-1 text-xs font-medium text-neutral-600 hover:bg-gray-200 dark:bg-blue-600 dark:bg-opacity-40 dark:text-neutral-200 dark:hover:bg-blue-700 dark:hover:bg-opacity-40"
           onClick={() => setDescriptionHeight((prev) => (prev === 0 ? 'auto' : 0))}
         >
-          <div className="mr-1">📏 Rule</div>
+          <div className="mr-1">
+            <span
+              className={clsx(
+                'ease inline-block transform transition-all duration-500',
+                descriptionHeight === 0 ? 'rotate-0' : 'rotate-[60deg]',
+              )}
+            >
+              📏
+            </span>{' '}
+            Rule
+          </div>
         </div>
         <AnimateHeight duration={500} easing="ease" height={descriptionHeight}>
           <div className="rounded-md border-0 border-l-[5px] border-solid border-neutral-500 bg-gray-200 p-2 text-xs italic text-neutral-600 dark:border-gray-200 dark:bg-neutral-600 dark:text-gray-200 [&_p]:mb-0">
