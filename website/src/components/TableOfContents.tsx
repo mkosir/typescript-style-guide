60cee710d01b4adb63812c80993d38aeb47b96f7
mkosir <kosir.mar@gmail.com>
2025-01-17 09:39:40 +0100

    update docusaurus

--- website/src/components/TableOfContents.tsx
+++ website/src/components/TableOfContents.tsx
@@ -2,19 +2,27 @@ import type { TOCItem } from '@docusaurus/mdx-loader';
 import TOCInline from '@theme/TOCInline';
 import { useState } from 'react';
 
-export const TableOfContents = ({ items }: { items: ReadonlyArray<TOCItem> }) => {
+type TableOfContentsProps = {
+  items: ReadonlyArray<TOCItem>;
+};
+
+export const TableOfContents = ({ items }: TableOfContentsProps) => {
   const [isTocExpanded, setIsTocExpanded] = useState(false);
 
   return (
-    <div className="toc-inline -mt-2">
+    <nav aria-label="Table of contents" className="toc-inline -mt-2">
       <button
+        aria-controls="toc-content"
+        aria-expanded={isTocExpanded}
         className="rounded-md border-none px-2 py-1 text-[11px] hover:cursor-pointer"
         title={`${isTocExpanded ? 'Collapse' : 'Expand'} Table of Contents`}
         onClick={() => setIsTocExpanded((prev) => !prev)}
       >
         {isTocExpanded ? 'Collapse' : 'Expand'}
       </button>
-      <TOCInline maxHeadingLevel={isTocExpanded ? 3 : 2} minHeadingLevel={2} toc={items} />
-    </div>
+      <div id="toc-content">
+        <TOCInline maxHeadingLevel={isTocExpanded ? 3 : 2} minHeadingLevel={2} toc={items} />
+      </div>
+    </nav>
   );
 };
