97f2883aaf3ff89efb47d90aef79e1ca47f32b18
mkosir <kosir.mar@gmail.com>
2025-01-21 08:49:27 +0100

    update docusaurus

--- website/src/components/GithubStats.tsx
+++ website/src/components/GithubStats.tsx
@@ -1,18 +1,16 @@
+const IFRAME_ATTRS = {
+  height: 20,
+  width: 100,
+} as const;
+
+const getSrcUrl = (type: 'star' | 'fork') =>
+  `https://ghbtns.com/github-btn.html?user=mkosir&repo=typescript-style-guide&type=${type}&count=true&size=small` as const;
+
 export const GithubStats = () => {
   return (
     <div className="flex items-center gap-0.5">
-      <iframe
-        className="mr-10 md:mr-40 min-[997px]:mr-0" // Adjust because of swizzling - src/theme/NavbarItem/ComponentTypes.tsx
-        height={20}
-        src="https://ghbtns.com/github-btn.html?user=mkosir&amp;repo=typescript-style-guide&amp;type=star&amp;count=true&amp;size=small"
-        width={100}
-      />
-      <iframe
-        className="hidden min-[997px]:inline"
-        height={20}
-        src="https://ghbtns.com/github-btn.html?user=mkosir&amp;repo=typescript-style-guide&amp;type=fork&amp;count=true&amp;size=small"
-        width={100}
-      />
+      <iframe {...IFRAME_ATTRS} className="mr-10 md:mr-40 min-[997px]:mr-0" src={getSrcUrl('star')} />
+      <iframe {...IFRAME_ATTRS} className="hidden min-[997px]:inline" src={getSrcUrl('fork')} />
     </div>
   );
 };
