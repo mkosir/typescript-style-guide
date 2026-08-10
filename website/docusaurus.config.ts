cbbdd72b8ff201f76506de9abb6bb0af9d0e1d60
mkosir <kosir.mar@gmail.com>
2026-08-10 14:55:41 +0200

    update docusaurus

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -19,7 +19,12 @@ const config: Config = {
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
